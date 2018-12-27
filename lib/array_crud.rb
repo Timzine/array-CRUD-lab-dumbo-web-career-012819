b662320035fd2dc5658e472d0d0e675a20dd4c59
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-27 22:54:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  random = [cat, mouse, pen, dictionnary]
+  random = ["cat", "mouse", "pen", "dictionnary"]
 end
 
 def add_element_to_end_of_array(array, element)
